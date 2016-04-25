106972c221ffa4e21b4e67e20f80b113e20adadf
Rich Ramsland <rramsland@gmail.com>
2016-04-24 22:50:02 -0400

    Jquery Slide on fullscreen viewing

--- js/script.js
+++ js/script.js
@@ -58,11 +58,11 @@ var fullScreen;
         //$(".fullImage").attr('src',fullScreen);
         $(".fullImage").css('background','url("http://rramsland.github.io' + fullScreen + '") no-repeat scroll 50% 50% / contain padding-box border-box');
         $(".contentFade").show();
-        $(".fullImage").show();
+        $(".fullImage").slideDown();
     });
 
     function exitFullscreen(){
-        $('.fullImage').hide();
+        $('.fullImage').slideUp();
         $(".contentFade").hide();
     }
 
