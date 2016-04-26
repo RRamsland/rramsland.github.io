ecb0e1f90606c2f9f014d6ef6d01b8987db1cf32
Rich Ramsland <rramsland@gmail.com>
2016-04-26 01:08:13 -0400

    Deleted Old JS

--- js/script.js
+++ js/script.js
@@ -1,60 +1,16 @@
 $(document).ready(function(){
-	var thumbImage;
-	var FullImage;
-	var artID;
 
 	$(".menuButton").click(function(){
 		$(".navbar").slideToggle();
 	});
 
-
-	$('.artProjectThumb').click(function(){
-
-		var thumbImage = $(this).css('background-image');
-        var FullImage = thumbImage.replace('Art-Thumbs', 'Art');
-        
-        if ($(this).hasClass('FullView')) 
-        {
-            $(this).height($(this).find('.artProjectThumb').height());
-            $(this).removeClass('FullView');
-            $(".fadeBack").hide();
-        } 
-        else {
-            var artID ="#"+$(this).attr('id'); 
-            $(artID).css('background-image',FullImage);
-    		$(artID).on('load', function(){
-    			alert("Image Loaded");
-    		});
-
-            $(this).height('');
-            $(this).addClass('FullView');
-            $(".fadeBack").show();
-
-            
-        }
-	});
-
-	$('.fadeBack').click(function(){
-        $('.artProjectThumb').removeClass('FullView');
-        $(".fadeBack").hide();
-	});
-
-	$(document).keyup(function(e) {
-        if (e.keyCode == 27) { // escape key maps to keycode `27`
-            $('.artProjectThumb').removeClass('FullView');
-            $(".fadeBack").hide();
-        }
-	});
-
-
-var fullScreen;
+    var fullScreen;
+    var img = new Image;
+    img.src = $('.fullImage').css('background-image').replace(/url\(|\)$/ig, "");
+    var bgImgWidth = img.width;
 
     $('.artThumb').click(function(){
-
-
-
         fullScreen = $(this).attr('src').replace('Art-Thumbs','Art');
-
         //$(".fullImage").attr('src',fullScreen);
         $(".fullImage").css('background','url("http://rramsland.github.io' + fullScreen + '") no-repeat scroll 50% 50% / contain padding-box border-box');
         $(".contentFade").show();
@@ -67,20 +23,17 @@ var fullScreen;
     }
 
     $('.fullImage').click(function(){
-        exitFullscreen()
+        exitFullscreen();
     });
     $('.contentFade').click(function(){
-        exitFullscreen()
+        exitFullscreen();
     });
 
-
-
-
-
-
-
-
-
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+            exitFullscreen();
+        }
+    });
 
 
 });
